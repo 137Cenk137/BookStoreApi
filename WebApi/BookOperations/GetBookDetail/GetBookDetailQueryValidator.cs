2128e3dd16a90c8c004b0746069d7ae87f23941f
Cenk Kıran <cenk33m@gmail.com>
2024-03-20 17:36:12 +0300

    Book detail validations  was completed

---
+++ WebApi/BookOperations/GetBookDetail/GetBookDetailQueryValidator.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using FluentValidation;
+using WebApi.Common;
+using WebApi.DBoperitions;
+
+namespace WebApi.BookOperations.GetBookDetail;
+
+public class GetBookDetailQueryValidator : AbstractValidator<GetBookDetailQuery>
+{
+    
+    public GetBookDetailQueryValidator(BookStoreDBContext dbContext)
+    {
+
+        RuleFor(query => query.BookId).NotEmpty().GreaterThan(0).LessThan(dbContext.Books.Count()+1);
+    }
+}
