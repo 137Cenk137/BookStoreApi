a768f817e3bdcb75103fc1e2580c3a3a56ea12b8
Cenk Kıran <cenk33m@gmail.com>
2024-03-20 17:37:05 +0300

    Update book Validations was written

---
+++ WebApi/BookOperations/UpdateBook/UpdateBookCommandValidator.cs
@@ -0,0 +1,17 @@
+
+using FluentValidation;
+using WebApi.DBoperitions;
+
+namespace WebApi.BookOperations.UpdateBook;
+
+public class UpdateBookCommandValidator : AbstractValidator<UpdateBookCommand>
+{
+    public UpdateBookCommandValidator()
+    {
+        RuleFor(command => command.Id).NotEmpty().GreaterThan(0);
+        RuleFor(command => command.Model.Title).NotEmpty().MinimumLength(4);
+        RuleFor(command => command.Model.GenreId).NotEmpty().GreaterThan(0).LessThan(4);
+        
+                
+    }
+}
