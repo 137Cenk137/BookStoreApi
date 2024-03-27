5594f3ae3e1709841972a314d26ca31ae78db780
Cenk Kıran <cenk33m@gmail.com>
2024-03-27 10:24:22 +0300

    CustomExceptionMiddleware was modified

--- WebApi/MiddleWare/CustomExceptionMiddleware.cs
+++ WebApi/MiddleWare/CustomExceptionMiddleware.cs
@@ -1,16 +1,19 @@
 using System.Diagnostics;
 using System.Net;
 using Newtonsoft.Json;
+using WebApi.Services;
 
 namespace WebApi.MiddleWare;
 
 public class CustomExceptionMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly ILoggerService _logger;
 
-    public CustomExceptionMiddleware(RequestDelegate next)
+    public CustomExceptionMiddleware(RequestDelegate next, ILoggerService logger)   
     {
         _next = next;   
+        _logger = logger;
     }
     public async Task Invoke(HttpContext context) 
     {
@@ -22,12 +25,13 @@ public class CustomExceptionMiddleware
             
             string message = "[Request] HTTP " + context.Request.Method.ToString() + "-" + context.Request.Path.ToString();
 
-            Console.WriteLine(message);
+            //Console.WriteLine(message);
+            _logger.Write(message);
             await _next(context);
 
             watch.Stop();
-            message = "[Request] HTTP " + context.Request.Method.ToString() + "-" + context.Request.Path.ToString() + " responded " + context.Response.StatusCode.ToString()+" in "+ watch.Elapsed.Milliseconds+" ms";
-            Console.WriteLine(message);
+            message = "[Response] HTTP " + context.Request.Method.ToString() + "-" + context.Request.Path.ToString() + " responded " + context.Response.StatusCode.ToString()+" in "+ watch.Elapsed.Milliseconds+" ms";
+           _logger.Write(message); 
 
          }
         catch (Exception ex )
@@ -44,7 +48,7 @@ public class CustomExceptionMiddleware
 
 
         string message = "[Error] HTTP " + context.Request.Method + " - " + context.Response.StatusCode + " Error Message "+  ex.Message + " in " + watch.ElapsedMilliseconds + " ms";
-        Console.WriteLine(message);
+       _logger.Write(message);
 
        
         var result = JsonConvert.SerializeObject(new{error = ex.Message},Formatting.None);
