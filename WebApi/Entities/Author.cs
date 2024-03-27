b9a67eff9b294b8b6f1d30a94e7502b6dc4e664e
Cenk Kıran <cenk33m@gmail.com>
2024-03-27 21:13:04 +0300

    Author classs was written

---
+++ WebApi/Entities/Author.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi;
+
+public class Author
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int AuthorId { get; set; }
+    public string Name { get; set;}
+    public string SurName { get; set; }
+    public DateTime Birthdate { get; set; }
+
+}
