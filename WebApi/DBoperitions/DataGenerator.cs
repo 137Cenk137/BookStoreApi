7e43944073cdc0456b74d952a897a28ec0da34f2
Cenk Kıran <cenk33m@gmail.com>
2024-03-28 20:30:43 +0300

    Author was added

--- WebApi/DBoperitions/DataGenerator.cs
+++ WebApi/DBoperitions/DataGenerator.cs
@@ -22,7 +22,12 @@ public class DataGenerator{
             new Book { Title="Think and grow rich",GenreId = 1,PageCount = 200,PublishDate = new DateTime(2001,06,12)},
             new Book { Title="Billoners",GenreId = 3,PageCount = 200,PublishDate = new DateTime(2001,06,12)},
             new Book { Title="Rich Dad,Poor Dad",GenreId = 1,PageCount = 200,PublishDate = new DateTime(2001,06,12)}
-        ); 
+        );
+
+            context.Authors.AddRange(
+                new Author(){Name = "Mustafa", SurName = "Kutlu",Birthdate = new DateTime(1999,0,12)},
+                new Author(){Name = "Sabahattin",SurName = "Ali",Birthdate = new DateTime(1907,02,25)}
+            );
             context.SaveChanges();
 
         }
